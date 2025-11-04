12e57552f2537df9e5056277f10f1f9bdfba8074
jfmossing <jfmossing@gmail.com>
2025-11-04 17:04:11 -0600

    Update Python_5.py

--- Python_5.py
+++ Python_5.py
@@ -1,7 +1,5 @@
 
-# Problem 1 (2 points)
-# Assign the 'name' variable an object that is your name of type str.
-name = "Josh Mossing"
+
 
 # Problem 2 (2 points)
 # Create a class and implement it for your problem of interest
@@ -17,7 +15,7 @@ class Worker:
     def get_email(self):
         return '{}{}@gmail.com'.format(self.first, self.last)
 
-# Problem 3 (2 points)
+
 # Create another class and implement it for your problem of interest
 class BlueCollar(Worker):
     def __init__ (self, first, last, salary,job:str, years_worked: int):
@@ -52,7 +50,6 @@ class BlueCollar(Worker):
         return int(applied_raise)
             
             
-# Problem 4 (2 points)
 # Create another class and implement it for your problem of interest
 
 class Manager(Worker): 
@@ -84,14 +81,11 @@ class Manager(Worker):
         return int(applied_bonus)
 # If you need to, you can create any additional classes or functions here as well.
 
-
-# Problem 5 (2 points)
 # Assign a variable named 'obj_1' an example instance of one of your classes
 obj_1 = Worker("Jim", "Lahey", 50000)
 
 # print(obj_1.get_name())
 
-# Problem 6 (2 points)
 #  Assign a variable named 'obj_2' an example instance of another one of your
 #  classes
 obj_2 = Manager("Steve","Jobs",100000, 30)
@@ -100,19 +94,11 @@ obj_2 = Manager("Steve","Jobs",100000, 30)
 # print(obj_2.apply_bonus())
 
 
-# Problem 7 (2 points)
 #  Assign a variable named 'obj_3' an example instance of one of your classes
 #  that extends another class
 obj_3= BlueCollar("Billy", "Buckenmeyer", 70000, "construction", 15)
 # print(obj_3.apply_raise())
 
-# Problems 8 through 14 are worth 4 points each.
-#    For each problem you must implement a test method in the following
-#     TestCase class. Each method name should be unique and start with 'test_'.
-#     Each method should test something different about the classes you created
-#     above. Use unittest.TestCase's assert methods to check your implementation.
-#     You will get 2 points for each test method created. You will get 2
-#     additional points for each of these tests that complete successfully.
 #     See https://docs.python.org/3/library/unittest.html for examples.
 
 import unittest
@@ -123,14 +109,12 @@ import inspect
 ###################################################
 class MyTestCases(unittest.TestCase):
 
-    # Problem 8
     # add test case method
     def test_get_name(self):
         worker1 = Worker("Bob", "Lahey", 50000)
         get_name_case1 = worker1.get_name()
         self.assertEqual(get_name_case1, "Bob Lahey")
           
-    # Problem 9
     # add test case method
 
     def test_get_job(self):
@@ -138,20 +122,18 @@ class MyTestCases(unittest.TestCase):
         get_job_case1 = bluecollar1.get_job()
         self.assertEqual(get_job_case1, "carpenter for 37 years")
            
-    # Problem 10
     # add test case method
     def test_apply_raise(self):
         bluecollar2 = BlueCollar("Jim", "Mossing", 85000, "plumber", 18)
         apply_raise_case1 = bluecollar2.apply_raise()
         self.assertEqual(apply_raise_case1, 93500)
            
-    # Problem 11
     # add test case method
     def test_apply_bonus(self):
         manager1 = Manager("Tony","Mossing", 140000, 75)
         apply_bonus_case1 = manager1.apply_bonus()
         self.assertEqual(apply_bonus_case1, 145000)
-    # Problem 12
+  
     # add test case method
     
     def test_get_email(self):
@@ -159,7 +141,7 @@ class MyTestCases(unittest.TestCase):
         get_email_case1 = worker2.get_email()
         self.assertEqual(get_email_case1,"PhilipJohnson@gmail.com")
         
-    # Problem 13
+
     # add test case method
     
     def test_get_raise(self):
@@ -167,7 +149,7 @@ class MyTestCases(unittest.TestCase):
         get_raise_case1 = bluecollar3.get_raise()
         self.assertEqual(get_raise_case1, 1.04)
         
-    # Problem 14
+  
     # add test case method
     def test_get_bonus(self):
         manager2 = Manager("Stephen", "Jobs", 120000, 26) 
@@ -228,4 +210,5 @@ class MyTestCases(unittest.TestCase):
 ##################################################
 if __name__ == "__main__":
     unittest.main()
+
 
